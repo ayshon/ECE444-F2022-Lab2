cb691edd78c3d698f83e58e2b013f71f023f344a
Sean <seanpatrickllera@yahoo.ca>
2022-09-26 16:49:38 -0400

    Integrated Bootstrap and jinja2. Added templates folder for jinja2. Created html templates for the index and user/name page.

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -7,4 +10,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html',name=name)

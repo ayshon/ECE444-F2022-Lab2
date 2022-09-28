ccae521cd3f12aa85521b7535a51de2ddfab58d7
Sean <seanpatrickllera@yahoo.ca>
2022-09-28 13:56:29 -0400

    Added an email address field and a message that displays only utoronto emails.

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from operator import truediv
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Regexp
 
 
 app = Flask(__name__)
@@ -15,6 +16,8 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?',validators=[DataRequired()])
+    emailAddress = EmailField('What is your UofT Email address?',validators=[DataRequired()])
+    # Regexp('\w?@.?utoronto.?',message='Please enter a UofT email')
     submit = SubmitField('Submit')
 
 
@@ -23,12 +26,21 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
-            flash('looks like you changed your name!')
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.emailAddress.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.emailAddress.data
+        substring = 'utoronto'
+        if substring in form.emailAddress.data:
+            session['isValidEmail'] = True
+        else:
+            session['isValidEmail'] = False
         return redirect(url_for('index'))
     # return render_template('index.html', current_time=datetime.utcnow())
-    return render_template('index.html', name=session.get('name'), form=form)
+    return render_template('index.html', name=session.get('name'), form=form, emailAddress=session.get('email'), isValidEmail=session.get('isValidEmail'))
 
 @app.route('/user/<name>')
 def user(name):
